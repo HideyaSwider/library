5791ab592b1babbecd9f1b582594171de094a66d
HideyaSwider <hideyaswider@gmail.com>
2017-10-23 10:57:03 -0400

    Input styled and queries the server on ENTER

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  state = {
+    search: ''
+  }
+
+  handleInputChange = ({ target }) => {
+    this.setState({ search: target.value })
+  }
+
+  handleSearch = e => {
+    if (e.keyCode === 13) {
+      fetch(`http://openlibrary.org/search.json?q=${this.state.search}`)
+        .then(resp => resp.json())
+        .then(json => {
+          //do something with json
+        })
+    }
+  }
+
   render() {
     return (
       <div className="app">
-        <p>Input here</p>
+        <input
+          className='search'
+          type="text"
+          onKeyUp={this.handleSearch}
+          value={this.state.value}
+          onChange={this.handleInputChange}
+          placeholder="Search books..."
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
